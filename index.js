7e9df6a3dc8ba934b68a44d239387c253a01ca20
zhipenglin <zhipeng.lin@leapin.io>
2025-01-23 11:40:25 +0800

    修改报错

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
-import fp from 'fastify-plugin';
-
-const dataFormat = fp(
-  async function (fastify, options) {
-    const { codeName, dataName, msgName, codePassValue } = Object.assign(
-      {},
-      {
-        codeName: 'code',
-        dataName: 'data',
-        msgName: 'msg',
-        codePassValue: 0
-      },
-      options
-    );
-    fastify.addHook('onSend', async (request, reply, payload) => {
-      const contentType = reply.getHeader('content-type');
-      if (contentType && payload && contentType.indexOf('application/json') > -1) {
-        let responseData;
-        try {
-          responseData = JSON.parse(payload);
-        } catch (e) {
-          return {
-            [codeName]: codePassValue,
-            [dataName]: payload
-          };
-        }
-        if (responseData.statusCode && (responseData.message || responseData.error)) {
-          return JSON.stringify({
-            [codeName]: Number.isInteger(Number(responseData.code)) ? Number(responseData.code) : responseData.statusCode || 500,
-            [msgName]: responseData.message || responseData.error
-          });
-        }
-        return JSON.stringify({
-          [codeName]: codePassValue,
-          [dataName]: JSON.parse(payload)
+const fp = require('fastify-plugin');
+module.exports = fp(
+    async function (fastify, options) {
+        const {codeName, dataName, msgName, codePassValue} = Object.assign(
+            {},
+            {
+                codeName: 'code',
+                dataName: 'data',
+                msgName: 'msg',
+                codePassValue: 0
+            },
+            options
+        );
+        fastify.addHook('onSend', async (request, reply, payload) => {
+            const contentType = reply.getHeader('content-type');
+            try {
+                if (contentType && typeof payload !== 'object' && contentType.indexOf('application/json') > -1) {
+                    const responseData = JSON.parse(String(payload));
+                    if (responseData.statusCode && (responseData.message || responseData.error)) {
+                        return JSON.stringify({
+                            [codeName]: Number.isInteger(Number(responseData.code)) ? Number(responseData.code) : responseData.statusCode || 500,
+                            [msgName]: responseData.message || responseData.error
+                        });
+                    }
+                    return JSON.stringify({
+                        [codeName]: codePassValue,
+                        [dataName]: JSON.parse(payload)
+                    });
+                }
+            } catch (e) {
+            }
+            return payload;
         });
-      }
-      return payload;
-    });
-  },
-  {
-    name: 'fastify-response-data-format'
-  }
+    },
+    {
+        name: 'fastify-response-data-format'
+    }
 );
-
-export default dataFormat;
